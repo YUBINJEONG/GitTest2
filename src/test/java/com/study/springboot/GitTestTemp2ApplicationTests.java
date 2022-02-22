b77e9d553dd818642af9d02cb8f7b44e0ccf170d
YUBINJEONG <jungyubin@gmail.com>
2022-02-23 07:17:01 +0900

    초기버전

---
+++ src/test/java/com/study/springboot/GitTestTemp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestTemp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
